5a8972c3c5d5ee15c29bdf88020877c173ebe119
Mr_shu <948929519@qq.com>
2022-05-10 14:22:30 +0800

    增加LinkListStack

---
+++ src/com/study/stack/LinkListStack.java
@@ -0,0 +1,48 @@
+package com.study.stack;
+
+import com.study.linklist.LinkList;
+
+/**
+ * 用链表实现的栈
+ * @param <E>
+ */
+public class LinkListStack<E> implements Stack<E> {
+    private LinkList<E> list;
+
+    public LinkListStack() {
+        list = new LinkList<>();
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+        list.addFirst(e);
+    }
+
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
+
+    @Override
+    public E peek() {
+        return list.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Stack top:");
+        res.append(list);
+        return res.toString();
+    }
+}
--- src/com/study/stack/Main.java
+++ src/com/study/stack/Main.java
@@ -1,13 +1,53 @@
 package com.study.stack;
 
+import com.study.queue.ArrayQueue;
+import com.study.queue.LoopQueue;
+import com.study.queue.Queue;
+
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
+    //    public static void main(String[] args) {
+//        ArrayStack<Integer> stack = new ArrayStack<>();
+//        for (int i = 0; i < 5; i++) {
+//            stack.push(i);
+//            System.out.println(stack);
+//        }
+//        stack.pop();
+//        System.out.println(stack);
+//    }
+
+//    public static void main(String[] args) {
+//        LinkListStack<Integer> stack = new LinkListStack<>();
+//        for (int i = 0; i < 5; i++) {
+//            stack.push(i);
+//            System.out.println(stack);
+//        }
+//        stack.pop();
+//        System.out.println(stack);
+//    }
+
+    private static double testStack(Stack<Integer> q, int opCount) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.push(random.nextInt(Integer.MAX_VALUE));
         }
-        stack.pop();
-        System.out.println(stack);
+        for (int i = 0; i < opCount; i++) {
+            q.pop();
+        }
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+        int opCount = 400000;
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        LinkListStack<Integer> linkListStack = new LinkListStack<>();
+        double time2 = testStack(linkListStack, opCount);
+        System.out.println("ArrayStack: "+time1 + "s");
+        System.out.println("LinkListStack: "+time2 + "s");
     }
 }
