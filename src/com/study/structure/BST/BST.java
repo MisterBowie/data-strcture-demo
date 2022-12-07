6126a79eeaf4796270815ad0718f1ed3a440c87d
舒先生 <948929519@qq.com>
2022-12-07 22:33:14 +0800

    BST前序 中序  后序遍

--- src/com/study/structure/BST/BST.java
+++ src/com/study/structure/BST/BST.java
@@ -72,4 +72,47 @@ public class BST<E extends Comparable> {
             return contains(node.right, e);
         }
     }
+
+    //前序遍历
+    public void preOrder(){
+        preOrder(root);
+    }
+
+    private void preOrder(Node node){
+        if (node == null){
+            return;
+        }
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+    //中序遍历特点： 可以让数据按照顺序排序
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    private void inOrder(Node node){
+        if (node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+    public void traverse(){
+        traverse(root);
+    }
+
+    private void traverse(Node node){
+        if (node == null){
+            return;
+        }
+        traverse(node.left);
+        traverse(node.right);
+        System.out.println(node.e);
+    }
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
